99bc62fb9b18f0bc58137d6b6012eb530ab39636
Erland <erland.handegard@outlook.com>
2023-01-13 10:12:15 +0100

    småtteri

--- player_class.py
+++ player_class.py
@@ -1,6 +1,10 @@
 class Tank:
-    def __init__(self, controls):
-        pass
+    def __init__(self, x, y, speed, keySet):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.keySet = keySet
+
 
 class Red(Tank):
     pass
