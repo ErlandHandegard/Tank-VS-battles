19f022ff16b96ebf2f4713fd7683bbc022aad431
Erland <erland.handegard@outlook.com>
2023-01-13 09:42:21 +0100

    Endre til jens sin måte

---
+++ classes.py
@@ -0,0 +1,9 @@
+class Tank:
+    def __init__(self):
+        pass
+
+class Red(Tank):
+    pass
+
+class Blue(Tank):
+    pass
---
+++ index.py
@@ -0,0 +1,2 @@
+import pygame as pg
+from pygame.locals import (K_UP, K_DOWN, K_LEFT, K_RIGHT)
