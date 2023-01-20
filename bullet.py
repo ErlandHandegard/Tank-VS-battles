0fd500e14289bd3711515ee0fcd8319ea5836b65
Erland <erland.handegard@outlook.com>
2023-01-20 09:59:56 +0100

    Lagt til bullet class

---
+++ bullet.py
@@ -0,0 +1,36 @@
+import pygame
+from pygame.locals import *
+import math
+
+class Bullet:
+    def __init__(self, window, x, y, angle, image):
+        self.image = pygame.image.load(image)
+        self.speed = 20
+        self.window = window
+        self.angle = angle
+        self.x = x
+        self.y = y
+        self.rect = pygame.Rect(self.x, self.y, self.image.get_rect()[2], self.image.get_rect()[3])
+
+    def fire(self, obstacles, tanks):
+        flying = True
+        while flying:
+            self.x += self.speed * math.cos(self.angle * (math.pi/180))
+            self.y += self.speed * -math.sin(self.angle * (math.pi/180))
+
+            if self.x > pygame.display.get_surface().get_size()[0] or self.x < 0:
+                del self
+            elif self.y > 0 or self.y < pygame.display.get_surface().get_size()[1]:
+                del self
+
+            for tank in tanks:
+                if self.rect.colliderect(tank.ract):
+                    #Slette tanken bulleten og sette score eventuelt skape en eksplosjon
+                    flying = False
+
+    
+            for obstacle in obstacles:
+                if self.rect.colliderect(obstacle.rect):
+                    #Slette instansen eventluelt lage en eksposjon
+                    flying = False
+
